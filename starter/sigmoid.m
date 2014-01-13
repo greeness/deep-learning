804bc3c6c6de80970a73d3ce990687ad91f3d438
Yang Gu <greeness@gmail.com>
2014-01-13 11:28:15 -0800

    finish of auto encoder ex.

---
+++ starter/sigmoid.m
@@ -0,0 +1,9 @@
+%-------------------------------------------------------------------
+% Here's an implementation of the sigmoid function, which you may find useful
+% in your computation of the costs and the gradients.  This inputs a (row or
+% column) vector (say (z1, z2, z3)) and returns (f(z1), f(z2), f(z3)). 
+
+function sigm = sigmoid(x)
+  
+    sigm = 1 ./ (1 + exp(-x));
+end
